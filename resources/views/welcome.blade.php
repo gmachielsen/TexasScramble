a9b2c26592bce5f20d6589917601efdec672982d
gmachielsen <g.machielsen@gmail.com>
2020-10-23 20:13:15 +0200

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,10 +22,9 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             <nav>
                 @if (Route::has('login'))
-                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    <div class="fixed top-0 right-0 px-6 py-4 sm:block">
                         @auth
                             <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
                         @else
@@ -51,6 +50,5 @@
 
 
 
-        </div>
     </body>
 </html>
