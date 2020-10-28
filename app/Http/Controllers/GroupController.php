f09a6c31756d0c726823608726e18663fde377a6
gmachielsen <g.machielsen@gmail.com>
2020-10-28 16:14:57 +0100

    initial commit

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -23,19 +23,75 @@ class GroupController extends Controller
         $players = [];
         $array = [];
         $array = request('Player');
-        $howManyGroups = request('NumberOfGroups');
+        
+        $int = request('NumberOfGroups');
+        $group = intval($int);
         $minimumMembersGroup = request('MinimumNumber');
         $maximumMembersGroup = request('MaximumNumber');
-        $totalhandicap;
+        $total = Null;
+        $handicaps = []; // gesorteerd van hoog naar laag;
 
 
-        foreach($array as $player) {
+
+
+        foreach($array as $player) 
+        {
+            // berekenen van de totale handicap van alle geselecteerde spelers samen. 
+            // vinden van de spelers in de database en dit stoppen in een array players.
             $players[] = Player::where('id', $player)->get();
+            $handicap = Player::where('id', $player)->firstOrFail('handicap')->handicap;
+            $handi = Player::where('id', $player)->firstOrFail('handicap')->handicap;
+            array_push($handicaps, $handi);
+
+            $total = $total + $handicap; 
         }
 
+        $avaragehandicap = $total / $group;
+
+        arsort($handicaps);
+        $numbers[] = intval($handicaps); // proberen handicap string in een nummber te converteren zodat ik groepen kan vormen
+
+        $flights = [];
+        $rest = [];
+        $flightsum = Null;
+        // groepen vormen
+        foreach($handicaps as $handicap)
+        {
+
+            foreach($flights as $flight) {
+                $flightsum += $flight;  
+
+            }
+            dd($flightsum);
+
+            if ($handicap + $flightsum < $avaragehandicap) 
+                {
+                    array_push($flights, $handicap);
+
+                } else {
+                    $rest[] = $handicap;
+                }
 
 
-        dd($players);
+        }
+   
+        dd($total, $group, $avaragehandicap, $handicaps, $flights, $numbers, $rest);
+
+    //    foreach($array as $player)
+    //     {
+    //         $handicap = Player::where('id', $player)->firstOrFail('handicap')->handicap;
+    //         if ($handicap < $avaragehandicap) {
+    //             dd($handicap);
+    //             $group .=  $handicap;
+    //             dd($group);
+
+    //         }
+    //     }
+
+
+
+    
+
 
         return redirect()->back();
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,18 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://use.fontawesome.com/releases/v5.7.2/css/solid.css" integrity="sha384-r/k8YTFqmlOaqRkZuSiE9trsrDXkh07mRaoGBMoDcmA58OHILZPsk29i2BsFng1B" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/fontawesome.css" integrity="sha384-4aon80D8rXCGx9ayDt85LbyUHeMWd3UiBaWliBlJ53yzm9hqN21A+o1pqoyK04h+" crossorigin="anonymous">
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/06a651c8da.js" crossorigin="anonymous"></script>
+    <!-- <link rel="stylesheet" type="text/css" href="{{ asset('dashboard_files/css/main.css')}}"> -->
+
     @stack('styles')
 
 </head>
@@ -37,7 +48,9 @@
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
-
+                    <div class="logo text-center px-6 center">
+                        <img src="logo/500_F_287005665_19go3Rha5RR1VZnS7JEHpG11nZ88KSmC.jpg" alt="">
+                    </div>
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
@@ -74,9 +87,17 @@
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+        <div class="container-fuid">
+        @yield('content')
+        @extends('footer')
+        </div>
+
+
+<!-- 
+        <script src="{{ asset('dashboard_files/js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{ asset('dashboard_files/js/popper.min.js')}}"></script>
+    <script src="{{ asset('dashboard_files/js/bootstrap.min.js')}}"></script>
+    <script src="{{ asset('dashboard_files/plugins/select2/select2.min.js')}}"></script> -->
     </div>
 </body>
 </html>
